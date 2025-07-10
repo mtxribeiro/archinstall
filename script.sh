d56147b9f11ee11c271cd358b9e9736ba47d9212
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-07-10 01:04:59 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav
-sudo pacman -S --needed --noconfirm wireplumber pipewire pipewire-alsa pipewire-jack pipewire-pulse pavucontrol
-sudo pacman -S --needed --noconfirm gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit-kde-agent gnome-keyring xdg-user-dirs xdg-user-dirs-gtk
-sudo pacman -S --needed --noconfirm noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra ttf-noto-nerd
-sudo pacman -S --needed --noconfirm ffmpeg xdg-utils git base-devel curl wget zip unzip mpv imv
+sudo pacman -S --needed --noconfirm \
+gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
+wireplumber pipewire pipewire-alsa pipewire-jack pipewire-pulse pavucontrol \
+gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
+xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit-kde-agent gnome-keyring xdg-user-dirs xdg-user-dirs-gtk \
+noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra ttf-noto-nerd \
+ffmpeg git base-devel xdg-utils curl wget zip unzip mpv imv
 command -v yay &>/dev/null || (cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ~ && rm -rf /tmp/yay)
 xdg-user-dirs-update
 sudo pacman -Scc --noconfirm
